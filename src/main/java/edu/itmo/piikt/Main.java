06e9c55fd8c70614c5cf267e614e9ca216a55d17
al-ek-sa <alexandra.lishik@gmail.com>
2026-02-22 02:22:07 +0300

    Ctrl+C Ctrl+Z

--- src/main/java/edu/itmo/piikt/Main.java
+++ src/main/java/edu/itmo/piikt/Main.java
@@ -5,9 +5,17 @@ import edu.itmo.piikt.io.IOProvider;
 import edu.itmo.piikt.io.IOScanner;
 import edu.itmo.piikt.reader.CSVParser;
 import edu.itmo.piikt.reader.HistorySave;
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
 
 public class Main {
     public static void main(String[] args) {
+        Signal.handle(new Signal("INT"), new SignalHandler () {
+            public void handle(Signal sig) {}
+        });
+        Signal.handle(new Signal("TSTP"), new SignalHandler () {
+            public void handle(Signal sig) {}
+        });
         IOProvider io = new IOScanner();
         HistorySave.getInstance().readFile();
         CSVParser csvParser = new CSVParser(io);
