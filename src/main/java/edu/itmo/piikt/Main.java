881bbce4fe6bbcdcda939fb3db211021563c1b20
al-ek-sa <alexandra.lishik@gmail.com>
2026-03-11 16:33:24 +0300

    signal

--- src/main/java/edu/itmo/piikt/Main.java
+++ src/main/java/edu/itmo/piikt/Main.java
@@ -1,31 +1,30 @@
 package edu.itmo.piikt;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import edu.itmo.piikt.command.modelCommand.HelpCommand;
+import edu.itmo.piikt.command.modelCommand.HelpEnteringCommand;
+import edu.itmo.piikt.io.provider.IOProvider;
+import edu.itmo.piikt.io.providerType.IOConsole;
+import edu.itmo.piikt.manager.ValidationCommand;
+import edu.itmo.piikt.saveManager.CSVParser;
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
+
+import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) {
-        /**
-         * Signal.handle(new Signal("INT"), SignalHandler.SIG_IGN); Signal.handle(new
-         * Signal("TSTP"), SignalHandler.SIG_IGN);
-         */
-        /**
-         * Logger logger = Logger.getLogger(Main.class.getName()); IOProvider io = new
-         * IOConsole(); // HistorySave.getInstance().readFile(); CSVParser csvParser =
-         * new CSVParser(); csvParser.readFile(io); HelpCommand help = new
-         * HelpCommand(); help.execute(io); HelpEnteringCommand helpEnteringCommand =
-         * new HelpEnteringCommand(); helpEnteringCommand.execute(io); ValidationCommand
-         * validationCommand = ValidationCommand.INSTANCE;
-         * validationCommand.validation(io);
-         */
-        try {
-            try (FileReader fileReader = new FileReader("workers.csv")) {
-                BufferedReader bufferedReaderider = new BufferedReader(fileReader);
-                System.out.println(bufferedReaderider.readLine());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        Signal.handle(new Signal("INT"), SignalHandler.SIG_IGN);
+        Signal.handle(new Signal("TSTP"), SignalHandler.SIG_IGN);
+        Logger logger = Logger.getLogger(Main.class.getName());
+        IOProvider io = new IOConsole();
+        // HistorySave.getInstance().readFile();
+        CSVParser csvParser = new CSVParser();
+        csvParser.readFile(io);
+        HelpCommand help = new HelpCommand();
+        help.execute(io);
+        HelpEnteringCommand helpEnteringCommand = new HelpEnteringCommand();
+        helpEnteringCommand.execute(io);
+        ValidationCommand validationCommand = ValidationCommand.INSTANCE;
+        validationCommand.validation(io);
     }
 }
