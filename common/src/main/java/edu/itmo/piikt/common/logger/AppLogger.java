3e2c70739ec6783d09881e453beeefadd3ad7083
al-ek-sa <alexandra.lishik@gmail.com>
2026-03-28 21:24:09 +0300

    logger

--- common/src/main/java/edu/itmo/piikt/common/logger/AppLogger.java
+++ common/src/main/java/edu/itmo/piikt/common/logger/AppLogger.java
@@ -3,10 +3,10 @@ package edu.itmo.piikt.common.logger;
 import ch.qos.logback.classic.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class AppLogger {
     private final Logger logger;
     private final String className;
+
     public AppLogger(Class<?> name) {
         this.className = name.getSimpleName();
         this.logger = (Logger) LoggerFactory.getLogger(name);
@@ -20,7 +20,7 @@ public class AppLogger {
 
     public void info(String message, Object... argument) {
         if (logger.isInfoEnabled()) {
-            logger.info("[{}] {}", className, String.format(message, argument));
+            logger.info("[{}] {}", className, format(message, argument));
         }
     }
 
@@ -32,7 +32,7 @@ public class AppLogger {
 
     public void error(String message, Object... argument) {
         if (logger.isErrorEnabled()) {
-            logger.error("[{}] {}", className, String.format(message, argument));
+            logger.error("[{}] {}", className, format(message, argument));
         }
     }
 
@@ -50,7 +50,7 @@ public class AppLogger {
 
     public void debug(String message, Object... argument) {
         if (logger.isDebugEnabled()) {
-            logger.debug("[{}] {}", className, String.format(message, argument));
+            logger.debug("[{}] {}", className, format(message, argument));
         }
     }
 
@@ -60,9 +60,9 @@ public class AppLogger {
         }
     }
 
-    public  void warn(String message, Object... argument) {
+    public void warn(String message, Object... argument) {
         if (logger.isWarnEnabled()) {
-            logger.warn("[{}] {}", className, String.format(message, argument));
+            logger.warn("[{}] {}", className, format(message, argument));
         }
     }
 
@@ -74,27 +74,15 @@ public class AppLogger {
 
     public void trace(String message, Object... argument) {
         if (logger.isTraceEnabled()) {
-            logger.trace("[{}] {}", className, String.format(message, argument));
+            logger.trace("[{}] {}", className, format(message, argument));
         }
     }
 
-    public boolean isDebugEnabled() {
-        return logger.isDebugEnabled();
-    }
-
-    public boolean isInfoEnabled() {
-        return logger.isInfoEnabled();
-    }
-
-    public boolean isTraceEnabled() {
-        return logger.isTraceEnabled();
-    }
-
-    public boolean isWarnEnabled() {
-        return logger.isWarnEnabled();
-    }
-
-    public boolean isErrorEnabled() {
-        return logger.isErrorEnabled();
+    private String format(String message, Object... argument) {
+        if (argument == null || argument.length == 0) {
+            return message;
+        }
+        String pattern = message.replace("{}", "%s");
+        return String.format(pattern, argument);
     }
 }
--- common/src/main/java/edu/itmo/piikt/common/logger/Context.java
+++ common/src/main/java/edu/itmo/piikt/common/logger/Context.java
@@ -4,31 +4,33 @@ import org.slf4j.MDC;
 
 import java.util.UUID;
 
-public class Context implements  AutoCloseable {
-    private static final String  KEY = "id";
+public class Context implements AutoCloseable {
+    private static final String KEY = "id";
+    private final String previousId;
 
     public Context() {
         this(UUID.randomUUID().toString());
     }
 
     public Context(String id) {
+        this.previousId = MDC.get(KEY);
         MDC.put(KEY, id);
     }
 
     @Override
-    public void  close() {
-        MDC.remove(KEY);
+    public void close() {
+        if (previousId != null) {
+            MDC.put(KEY, previousId);
+        } else {
+            MDC.remove(KEY);
+        }
     }
 
     public static String getId() {
-        return  MDC.get(KEY);
+        return MDC.get(KEY);
     }
 
     public static Context newId() {
         return new Context();
-    }
-
-    public static Context withId(String id) {
-        return new Context(id);
     }
 }
