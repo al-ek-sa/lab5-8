f9f315913cfc0e1994e419019c55bec3a2e1e795
al-ek-sa <alexandra.lishik@gmail.com>
2026-03-28 21:25:29 +0300

    rm

--- src/main/java/edu/itmo/piikt/common/data/MessageExceptionValidation.java
+++
@@ -1,7 +0,0 @@
-package edu.itmo.piikt.common.data;
-
-import java.io.Serializable;
-
-public record MessageExceptionValidation(String name, String message) implements Serializable {
-    private static final long serialVersionUID = 1L;
-}
